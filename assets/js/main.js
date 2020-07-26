ba4029104d008d2006db269b0f098c54c61b7393
atulbhatt-system32 <atulbhatt98@gmail.com>
2020-07-26 11:57:04 +0530

    loader speed changed

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@
 
 $(document).ready(function() {
   //Preloader
-  var preloaderFadeOutTime = 1000;
+  var preloaderFadeOutTime = 5000;
   function hidePreloader() {
   var preloader = $('.spinner-wrapper');
   preloader.fadeOut(preloaderFadeOutTime);
